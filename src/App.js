0e86b868a9f378aa0140d5e1becb4aa987cc60f6
bigdesire0210 <bigdesire210@gmail.com>
2021-02-13 10:47:33 +0900

    chatapp準備

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Login,SinUp,Roomコンポーネントを呼び出す
+import Login from './Login'
+import SignUp from './SignUp'
+import Room from './Room'
+
+const App = () => {
+    return (
+        <>
+            <Login />
+            <SignUp />
+            <Room />
+        </>
+    )
 }
 
-export default App;
+
+export default App
---
+++ src/Room.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//ログインコンポーンネント、サインアップコンポーネントを呼び出す
+import Login from './Login'
+import SignUp from './SignUp'
+
+const Room = () => {
+    return (
+        <>
+            <h1>Room</h1>
+        </>
+    )
+}
+
+
+export default Room
